f181ac18bee172f93563e000a6724fd5926eb317
henrykim12 <henrykim966@gmail.com>
2023-05-24 22:18:26 -0700

    eliminate slimes, but no slime animation

--- Assets/SwordAttack.cs
+++ Assets/SwordAttack.cs
@@ -4,27 +4,36 @@ using UnityEngine;
 
 public class SwordAttack : MonoBehaviour
 {
+    public Collider2D swordCollider;
+    public float damage = 3;
     Vector2 rightAttackOffset;
-    Collider2D swordCollider;
 
     private void Start() {
-        swordCollider = GetComponent<Collider2D>();
         rightAttackOffset = transform.position;
     }
 
     public void AttackRight() {
-        print("attack right");
         swordCollider.enabled = true;
-        transform.position = rightAttackOffset;
+        transform.localPosition = rightAttackOffset;
     }
 
     public void AttackLeft() {
-        print("attack left");
         swordCollider.enabled = true;
-        transform.position = new Vector3(rightAttackOffset.x * -1, rightAttackOffset.y);
+        transform.localPosition = new Vector3(rightAttackOffset.x * -1, rightAttackOffset.y);
     }
 
     public void StopAttack() {
         swordCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Enemy") {
+            // deal damage to the enemy
+            Enemy enemy = other.GetComponent<Enemy>();
+
+            if (enemy != null) {
+                enemy.Health -= damage;
+            }
+        }
     }
 }
